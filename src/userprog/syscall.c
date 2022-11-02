db85f79dc830f15249090ce804ba9e2e553cd5d8
Hamish McCreanor <hwm21@aube.doc.ic.ac.uk>
2022-11-02 10:33:22 +0000

    added file system lock for syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,16 +3,20 @@
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
+#include "threads/synch.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 static bool is_valid_user_ptr (void *uaddr);
 
+static struct lock fs_lock;
+
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  lock_init (&fs_lock);
 }
 
 static void
