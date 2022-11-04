4576d30493fcc98e9c138a45da6a79163e339d8c
Hamish McCreanor <hwm21@aube.doc.ic.ac.uk>
2022-11-04 12:45:34 +0000

    lg721 pm521 ccf20 hwm21 basic use rprocess implementation

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,5 +1,6 @@
 #include "userprog/syscall.h"
 #include "userprog/pagedir.h"
+#include "userprog/process.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
@@ -7,18 +8,20 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
-#define N_TASK_ONE_SYSCALLS 13
+/* There are 13 syscalls in task two. */
+#define N_SYSCALLS 13
+#define MAX_CONSOLE_BUFFER_OUTPUT 250
 
 static void syscall_handler (struct intr_frame *);
 static bool is_valid_user_ptr (void *uaddr);
 static void syscall_handlers_init (void);
 
-static void exit (struct intr_frame *f);
-static void write (struct intr_frame *f);
+static void exit_handler (struct intr_frame *f);
+static void write_handler (struct intr_frame *f);
 
 static struct lock fs_lock;
 
-static void (*syscall_ptrs[N_TASK_ONE_SYSCALLS]) (struct intr_frame *f);
+static void (*syscall_ptrs[N_SYSCALLS]) (struct intr_frame *f);
 
 void
 syscall_init (void) 
@@ -26,13 +29,14 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
   lock_init (&fs_lock);
   syscall_handlers_init ();
+  printf("syscall_init\n");
 }
 
 static void 
 syscall_handlers_init (void)
 {
   syscall_ptrs[SYS_HALT] = NULL;
-  syscall_ptrs[SYS_EXIT] = &exit;
+  syscall_ptrs[SYS_EXIT] = &exit_handler;
   syscall_ptrs[SYS_EXEC] = NULL;
   syscall_ptrs[SYS_WAIT] = NULL;
   syscall_ptrs[SYS_CREATE] = NULL;
@@ -40,7 +44,7 @@ syscall_handlers_init (void)
   syscall_ptrs[SYS_OPEN] = NULL;
   syscall_ptrs[SYS_FILESIZE] = NULL;
   syscall_ptrs[SYS_READ] = NULL;
-  syscall_ptrs[SYS_WRITE] = &write;
+  syscall_ptrs[SYS_WRITE] = &write_handler;
   syscall_ptrs[SYS_SEEK] = NULL;
   syscall_ptrs[SYS_TELL] = NULL;
   syscall_ptrs[SYS_CLOSE] = NULL;
@@ -50,29 +54,43 @@ static void
 syscall_handler (struct intr_frame *f) 
 {
   /* Do we need validation here to prevent syscalls outside the range of the syscall array? */
-  void (*fp) (struct intr_frame *) = syscall_ptrs[(int) f->esp];
-  if (fp)
-  {
-    fp (f);
-  } else {
-    printf ("Unimplemented syscall!\n");
-    thread_exit ();
+  int syscall_n = *((int *) f->esp);
+  if (is_valid_user_ptr (syscall_n)) {
+    void (*fp) (struct intr_frame *) = syscall_ptrs[*((int *) f->esp)];
+    printf("syscall_handler\n");
+    if (fp) {
+      printf("function pointer\n");
+      fp (f);
+    } else {
+      printf ("Unimplemented syscall!\n");
+      thread_exit ();
+    }
   }
 }
 
 static bool 
 is_valid_user_ptr (void *uaddr)
 {
-  return is_user_vaddr (uaddr) && pagedir_get_page (thread_current ()->pagedir, uaddr);
+  return uaddr && is_user_vaddr (uaddr) && pagedir_get_page (thread_current ()->pagedir, uaddr);
 }
 
 static void 
-exit (struct intr_frame *f) 
+exit_handler (struct intr_frame *f UNUSED) 
 {
+  process_exit ();
 }
 
 static void 
-write (struct intr_frame *f)
+write_handler (struct intr_frame *f)
 {
+  
+  int fd = *(((int *) f->esp) + 1);
+  char *buffer =  *((char **) f->esp + 2);
+  int size = *((int *) f->esp + 3);
+
+  if (fd == 1) {
+    printf("cameron smells good.\n");
+    putbuf (buffer, size);
+  }
 
 }
