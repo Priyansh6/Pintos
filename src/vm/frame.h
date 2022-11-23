8630476bd18ec32600d06baf7ebafdbebffc82b1
Lucas Graeff Buhl-Nielsen <lg721@ic.ac.uk>
2022-11-23 11:12:03 +0000

    call frame_table_get_frame in process.c instead of palloc_get_page (hwm21, lg721)

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,7 +1,9 @@
 #ifndef VM_FRAME_H
 #define VM_FRAME_H
 
+#include "threads/palloc.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 #define N_FRAMES ((4 << 20) / PGSIZE)
 
@@ -12,7 +14,7 @@ struct frame_table_entry {
 
 struct frame_table_entry *frame_table[N_FRAMES];
 
-void *frame_table_get_frame (void *upage);
-void frame_table_free_frame (uintptr_t frame_no);
+void *frame_table_get_frame (void *upage, enum palloc_flags flags);
+void frame_table_free_frame (void *kaddr);
 
 #endif
